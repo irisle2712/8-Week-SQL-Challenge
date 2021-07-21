6f606a413b07e722522948e18c59bd8d80f2c5db
irisle2712 <81307010+irisle2712@users.noreply.github.com>
2021-07-21 19:04:13 +0300

    Update week2-query-part2.sql

--- Case Study #2 - Pizza Runner/B. Runner and Customer Experience/week2-query-part2.sql
+++ Case Study #2 - Pizza Runner/B. Runner and Customer Experience/week2-query-part2.sql
@@ -25,6 +25,13 @@ FROM pizza_runner.runners
 GROUP BY registration_week
 ORDER BY registration_week;
 
+--Results:
+| registration_week        | runners_count |
+| ------------------------ | ------------- |
+| 2021-01-01T00:00:00.000Z | 2             |
+| 2021-01-08T00:00:00.000Z | 1             |
+| 2021-01-15T00:00:00.000Z | 1             |
+
 --Q2: What was the average time in minutes it took for each runner to arrive at the Pizza Runner HQ to pickup the order?
 -- Cast pickup_time field into a timestamp & Use DATE_PART()
 -- Use AGE func to extract number of minutes from the interval
@@ -41,6 +48,11 @@ SELECT
 	ROUND(AVG(pickup_minutes), 3) AS avg_pickup_min
 FROM cte_pickup_minutes;
 
+--Results:
+| avg_pickup_min |
+| -------------- |
+| 15.625         |
+
 --Q3: Is there any relationship between the number of pizzas and how long the order takes to prepare?
 SELECT 
 	DISTINCT(r.order_id),
@@ -53,6 +65,18 @@ WHERE r.pickup_time IS NOT NULL
 GROUP BY r.order_id, prepare_min
 ORDER BY r.order_id;
 
+--Results:
+| order_id | prepare_min | pizza_count |
+| -------- | ----------- | ----------- |
+| 1        | 10          | 1           |
+| 2        | 10          | 1           |
+| 3        | 21          | 2           |
+| 4        | 29          | 3           |
+| 5        | 10          | 1           |
+| 7        | 10          | 1           |
+| 8        | 20          | 1           |
+| 10       | 15          | 2           |
+
 --Q4: What was the average distance travelled for each customer?
 SELECT 
 	c.customer_id,
@@ -64,10 +88,24 @@ WHERE r.pickup_time IS NOT NULL
 GROUP BY c.customer_id
 ORDER BY c.customer_id;
 
+--Results:
+| customer_id | avg_distance |
+| ----------- | ------------ |
+| 101         | 20.00        |
+| 102         | 16.73        |
+| 103         | 23.40        |
+| 104         | 10.00        |
+| 105         | 25.00        |
+
 --Q5: What was the difference between the longest and shortest delivery times for all orders?
 SELECT 
 	MAX(duration) - MIN(duration) AS max_difference
 FROM updated_runner_orders;
+
+--Results:
+| max_difference |
+| -------------- |
+| 30             |
 
 --Q6: What was the average speed for each runner for each delivery and do you notice any trend for these values?
 SELECT 
@@ -81,6 +119,19 @@ FROM updated_runner_orders
 WHERE cancellation IS NULL
 ORDER BY order_id;
 
+--Results:
+
+| order_id | runner_id | hour_of_day | distance | duration | avg_speed |
+| -------- | --------- | ----------- | -------- | -------- | --------- |
+| 1        | 1         | 18          | 20       | 32       | 37.5      |
+| 2        | 1         | 19          | 20       | 27       | 44.4      |
+| 3        | 1         | 0           | 13.4     | 20       | 40.2      |
+| 4        | 2         | 13          | 23.4     | 40       | 35.1      |
+| 5        | 3         | 21          | 10       | 15       | 40.0      |
+| 7        | 2         | 21          | 25       | 25       | 60.0      |
+| 8        | 2         | 0           | 23.4     | 15       | 93.6      |
+| 10       | 1         | 18          | 10       | 10       | 60.0      |
+
 --Q7: What is the successful delivery percentage for each runner?
 SELECT 
 	runner_id,
@@ -90,3 +141,10 @@ SELECT
 FROM updated_runner_orders
 GROUP BY runner_id
 ORDER BY runner_id;
+
+--Results:
+| runner_id | successful_percentage |
+| --------- | --------------------- |
+| 1         | 100                   |
+| 2         | 75                    |
+| 3         | 50                    |
